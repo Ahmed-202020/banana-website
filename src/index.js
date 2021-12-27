544f31ea19fa6aad4105b8d3969914bdd93060dd
Ahmed-202020 <ak812554@gmail.com>
2021-12-27 18:46:18 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+let form = document.getElementById("frm") ; 
+let img = document.getElementById("img") ; 
+let sort = document.getElementById("sort") ;
+let sorting = document.getElementById("sorting") ; 
+
+form.addEventListener("focus" , function(){
+  img.setAttribute("src" , "./assets/icons/Search-black.svg") ; 
+});
+form.addEventListener("blur" , function(){
+  img.setAttribute("src" , "./assets/icons/Search.svg") ; 
+});
+form.addEventListener("focus" , function(){
+  sort.setAttribute("src" , "./assets/icons/Icon material-sort-green.svg") ; 
+});
+form.addEventListener("blur" , function(){
+  sort.setAttribute("src" , "./assets/icons/Icon material-sort.svg") ; 
+});
+
+$(function(){
+    $("#sort").on("click" , function(){
+      $("#sorting").fadeToggle() ; 
+    }) ; 
+    $(".common-products .btn").on("click" , function(){
+      Swal.fire(
+        "تم" , 
+        'تم اضافة المنتج الى عربة الشراء',
+        'success'
+      ) ; 
+    })
+})
+
+
+
