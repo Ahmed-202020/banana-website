12d02906af96c346a62a705a873317975d5b7b1f
Ahmed-202020 <ak812554@gmail.com>
2022-01-10 15:38:57 +0200

    the page orders.html has completed with responsive dsign and ltr dir

---
+++ src/orders.js
@@ -0,0 +1,30 @@
+$(function(){
+    $(".item").on("click" , function(){
+        $(".item").removeClass("active-orders") ; 
+        $(this).addClass("active-orders") ; 
+    })
+    $(function(){
+        $(".delete-order").on("click" , function(){
+            $(this).parentsUntil(".orders").fadeOut(200) ; 
+        })
+
+        $(".current-orders-item").on("click" , function(){
+            if($(".current-orders-item").hasClass("active-orders")){
+                $(".current-orders").show() ; 
+                $(".prev-orders").css({display: "none"}) ; 
+            }
+        });
+        $(".prev-orders-item").on("click" , function(){
+            if($(".prev-orders-item").hasClass("active-orders")){
+                $(".current-orders").css({display: "none"}) ; 
+                $(".prev-orders").show() ; 
+            }
+        });
+
+        $(".edition").on("click" , function(){
+            $(".orders").remove() ; 
+            $(this).text("حفظ") ;
+            $(this).css(({backgroundColor : "#ff3003"}))
+        })
+    }) ;
+});
