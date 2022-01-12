04ca90abe569f3465566368a1ad168cb2cb1c2cb
Ahmed-202020 <ak812554@gmail.com>
2022-01-12 03:13:30 +0200

    the last updating

--- src/product.js
+++ src/product.js
@@ -25,6 +25,11 @@ $(".subtract-2").on("click" , function(){
     }
 })
 
+
+// $(".notes #note").on("keypress" , function(){
+//     $(".holder").css("display" , "none") ;
+// })
+
 $(".hearty").on("click" , function(){
     $(this).toggleClass("active-fav");
 })
@@ -33,6 +38,23 @@ $(".delete-order").on("click" , function(){
     $(this).parentsUntil(".current-orders").fadeOut(200) ; 
 })
 
+$(".modal-confirm").on("click" , function(){
+    $(".modal-addresses").modal("show") ; 
+})
+
+$(".modal-edit").on("click" , function(){
+    $(".modal-edit-address").modal("show") ;
+        $(".modal-addresses").modal("hide") ; 
+})
+
+$(".another-address").on("click" , function(){
+    $(".modal-add-address").modal("show") ; 
+    $(".modal-addresses").modal("hide") ;
+})
+
+$(".delete").on("click" , function(){
+    $(this).parentsUntil(".addresses").fadeOut() ; 
+})
 
 $(".add-to-cart").on("click" , function(){
     Swal.fire(
